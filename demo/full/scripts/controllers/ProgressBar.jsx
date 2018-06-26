6ea33c6c1b42e4dc57550ded34d95eea5c056555
peaBerberian <pea.berberian@gmail.com>
2018-06-26 19:34:43 +0200

    demo: adapt demo to french hour (DO NOT MERGE)

--- demo/full/scripts/controllers/ProgressBar.jsx
+++ demo/full/scripts/controllers/ProgressBar.jsx
@@ -19,7 +19,7 @@ class Progressbar extends React.Component {
 
   showTimeIndicator(wallClockTime, clientX) {
     const date = new Date(wallClockTime * 1000);
-    const hours = date.getHours().toString().padStart(2, "0");
+    const hours = ((date.getUTCHours() + 2) % 24).toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
     const seconds = date.getSeconds().toString().padStart(2, "0");
     const currentReadableTime =  hours + ":" + minutes + ":" + seconds;
